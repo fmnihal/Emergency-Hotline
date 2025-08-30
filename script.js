06ed5f6cb8fb400e2f44a601df7482ba290d34b2
fmnihal <fattahmahmudnihal@gmail.com>
2025-08-30 12:38:57 +0600

    gave id to clickable options

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-document.getElementById()
+let heartCount= parseInt(document.getElementById('heart-count').innerText);
+let coinCount= parseInt(document.getElementById('coin-count').innerText);
+let copyCount= parseInt(document.getElementById('copy-count').innerText);
+
+// document.getElementById('heart-count').addEventListener('click', function(event){
+//     console.log(heartCount);
+// })
+// console.log(heartCount);
+// document.getElementsByClassName('tap-copy').addEventListener('click', function(event){
+//     // console.log(copyCount);
+// });
+
+
+// const hearts= document.getElementsByClassName('tap-heart');
+// const copys= document.getElementsByClassName('tap-copy');
+// const calls= document.getElementsByClassName('tap-call');
+// for (let heart of hearts){
+//     heart.addEventListener('click', function(event){
+//         heartCount++;
+//         // console.log(heartCount);
+//         document.getElementById('heart-count').innerText= heartCount;
+//     });
+// }
+// for (let copy of copys){
+//     copy.addEventListener('click', function(event){
+//         copyCount++;
+//         console.log(copyCount);
+//         document.getElementById('copy-count').innerText= copyCount;
+//     });
+// }
+// for (let call of calls){
+//     call.addEventListener('click', function(event){
+//         if(coinCount<20){
+//             alert("You don't have enough coins to make call");
+//         }
+//         else{
+//             coinCount-=20;
+//             // console.log(coinCount);
+//             document.getElementById('coin-count').innerText= coinCount;
+//         }
+//     });
+// }
+
